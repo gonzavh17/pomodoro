7b2356fcbf0f28fd0f70209d1ebea3a23197dd10
Gonzalo Asis <gonzaasis17@gmail.com>
2022-08-07 22:12:31 -0300

    agregando fondos y estilos

--- script.js
+++ script.js
@@ -55,7 +55,7 @@ function runner() {
 }
 
 function stopTimer() {
-    location.reload();
+    location.reload()
 }
 
 function finishAlert() {
@@ -69,20 +69,79 @@ function finishAlert() {
     });
 }
 
+// Creando funcion para guardar tareas dentro del local storage
+
+document.getElementById('formTask').addEventListener('submit', saveTask);
+
+function saveTask(e) {
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    console.log(description)
+
+    let task = {
+        title,
+        description
+    };
+
+    if (localStorage.getItem('tasks') === null) {
+        let tasks = [];
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    } else {
+        let tasks = JSON.parse(localStorage.getItem('tasks'));
+        tasks.push(task);
+        localStorage.setItem('tasks', JSON.stringify(tasks));
+    }
+
+    getTasks();
+    document.getElementById('formTask').reset();
+    e.preventDefault();
+}
+
+function deleteTask(title) {
+    console.log(title)
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    for (let i = 0; i < tasks.length; i++) {
+        if (tasks[i].title == title) {
+            tasks.splice(i, 1);
+        }
+    }
+
+    localStorage.setItem('tasks', JSON.stringify(tasks));
+    getTasks();
+}
+
+function getTasks() {
+    let tasks = JSON.parse(localStorage.getItem('tasks'));
+    let tasksView = document.getElementById('tasks');
+    tasksView.innerHTML = '';
+    for (let i = 0; i < tasks.length; i++) {
+        let title = tasks[i].title;
+        let description = tasks[i].description;
+
+        tasksView.innerHTML += `<div class="card mb-3">
+        <div class="card-body">
+          <p>${title} - ${description}
+          <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">Delete</a>
+          </p>
+        </div>
+      </div>`;
+    }
+}
+
+getTasks();
 
 // Introduciendo apis y generador de frase
 
-const frase = document.querySelector("#frase")
-const autor = document.querySelector("#autor")
-const buttonNuevaFrase = document.querySelector("#button-nueva-frase")
+const frase = document.querySelector("#frase");
+const autor = document.querySelector("#autor");
+const buttonNuevaFrase = document.querySelector("#button-nueva-frase");
 
-
-buttonNuevaFrase.addEventListener("click", getFrase)
+buttonNuevaFrase.addEventListener("click", getFrase);
 
 function getFrase() {
-
     let num = parseInt(Math.random() * 10);
-    console.log(num)
+    console.log(num);
 
     fetch("frases.json")
         .then((response) => response.json())
@@ -97,16 +156,16 @@ function getFrase() {
                 frase.innerHTML = `"${data[2].frase}"`;
                 autor.innerHTML = `-${data[2].autor}`;
             } else if (num === 3) {
-                4
+                4;
                 frase.innerHTML = `"${data[3].frase}"`;
                 autor.innerHTML = `-${data[3].autor}`;
             } else if (num === 4) {
-                5
+                5;
                 frase.innerHTML = `"${data[4].frase}"`;
                 autor.innerHTML = `-${data[4].autor}`;
             } else if (num === 5) {
                 frase.innerHTML = `"${data[5].frase}"`;
-                67
+                67;
                 autor.innerHTML = `-${data[5].autor}`;
             } else if (num === 6) {
                 frase.innerHTML = `"${data[6].frase}"`;
@@ -123,66 +182,3 @@ function getFrase() {
             }
         });
 }
-
-
-// Creando funcion para guardar tareas dentro del local storage
-
-document.getElementById("formTask").addEventListener("submit", saveTask);
-
-function saveTask(e) {
-    let title = document.getElementById("title").value;
-    let description = document.getElementById("description").value;
-    console.log(description);
-
-    let task = {
-        title,
-        description,
-    };
-
-    if (localStorage.getItem("tasks") === null) {
-        let tasks = [];
-        tasks.push(task);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    } else {
-        let tasks = JSON.parse(localStorage.getItem("tasks"));
-        tasks.push(task);
-        localStorage.setItem("tasks", JSON.stringify(tasks));
-    }
-
-    getTasks();
-    document.getElementById("formTask").reset();
-    e.preventDefault();
-}
-
-function deleteTask(title) {
-    console.log(title);
-    let tasks = JSON.parse(localStorage.getItem("tasks"));
-    for (let i = 0; i < tasks.length; i++) {
-        if (tasks[i].title == title) {
-            tasks.splice(i, 1);
-        }
-    }
-
-    localStorage.setItem("tasks", JSON.stringify(tasks));
-    getTasks();
-}
-
-function getTasks() {
-    let tasks = JSON.parse(localStorage.getItem("tasks"));
-    let tasksView = document.getElementById("tasks");
-    tasksView.innerHTML = "";
-    for (let i = 0; i < tasks.length; i++) {
-        let title = tasks[i].title;
-        let description = tasks[i].description;
-
-        tasksView.innerHTML += `<div class="card mb-3">
-        <div class="card-body">
-          <p>${title} - ${description}
-          <a href="#" onclick="deleteTask('${title}')" class="btn btn-danger ml-5">Delete</a>
-          </p>
-        </div>
-      </div>`;
-    }
-}
-
-getTasks();
